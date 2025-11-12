bb2c8d5402110da13670efa7b8eecbeda601ee17
Antonin Portelli <antonin.portelli@me.com>
2025-11-12 11:40:17 +0900

    moved results to dated directory and Apple Silicon fixes

--- systems/apple-silicon/files/env-cpu.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck disable=SC1091
-
-__script_dir__="$(dirname "$(readlink -f "${BASH_SOURCE:-$0}")")"
-
-source "${__script_dir__}/env-pixi.sh" cpu-apple-silicon
-export DYLD_FALLBACK_LIBRARY_PATH="${LD_LIBRARY_PATH}"
